0dfa0e12ce204b6b767af567bad777450d2563c6
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-04 11:20:21 -0800

    getting to params in the url

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const { id, name } = this.route.snapshot.params;
+    this.user = { id, name };
   }
 
 }
