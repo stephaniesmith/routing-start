8332767043cdf5428c1c9a591376600590fb7e60
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-04 11:26:56 -0800

    unsusbscribe on Destroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,6 +22,10 @@ export class UserComponent implements OnInit {
         this.user.id = params['id'];
         this.user.name = params['name'];
       });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
