d53fbcddac982808a998f369b2e08656ae22623f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-01-04 14:47:33 -0800

    auth service

---
+++ src/app/auth.service.ts
@@ -0,0 +1,21 @@
+import { reject } from "q";
+
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
